4eb34ff6157dfeae89247bd5020e159922914bf8
Barchuk Andrii <abarchuk1976@gmail.com>
2022-12-15 01:25:17 +0200

    commit

---
+++ src/components/profile/profile.jsx
@@ -0,0 +1,36 @@
+import PropTypes from 'prop-types';
+import './profile.css';
+
+export const Profile = ({ username, tag, location, avatar, stats }) => (
+  <div className="profile">
+    <div className="description">
+      <img src={avatar} alt="User avatar" className="avatar" />
+      <p className="name">{username}</p>
+      <p className="tag">@{tag}</p>
+      <p className="location">{location}</p>
+    </div>
+
+    <ul className="stats">
+      <li>
+        <span className="label">Followers</span>
+        <span class="quantity">{stats.followers}</span>
+      </li>
+      <li>
+        <span class="label">Views</span>
+        <span class="quantity">{stats.views}</span>
+      </li>
+      <li>
+        <span class="label">Likes</span>
+        <span class="quantity">{stats.likes}</span>
+      </li>
+    </ul>
+  </div>
+);
+
+Profile.propTypes = {
+  username: PropTypes.string,
+  tag: PropTypes.string,
+  location: PropTypes.string,
+  avatar: PropTypes.string,
+  stats: PropTypes.objectOf(PropTypes.number),
+};
